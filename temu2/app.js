3a84f90dba682cc2ec53fdea83d308c3205baf53
JovanNathanielJie <jovannathanieljie_2327240013@mhs.mdp.ac.id>
2025-09-17 13:03:21 +0700

    Update app.js

--- temu2/app.js
+++ temu2/app.js
@@ -52,7 +52,7 @@ app.get('/about', (req, res) => {
     res.send('<h1>Tentang Kami</h1><p>Halaman tambahan dengan HTML langsung.</p>');
 });
 
-// Jalankan serbver
+// Jalankan server
 app.listen(PORT, () => {
     console.log(`Server berjalan pada http://localhost:${PORT}`);
 });
